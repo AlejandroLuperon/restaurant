607fe3f53850877592295dc9044a76e560ad1446
Alejandro Luperon <alejandroluperon@Alejandros-MacBook-Air.local>
2019-04-12 18:41:14 -0700

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillReceiveProps(newProps) {
+
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div>Hello world.</div>
     );
   }
 }
-
 export default App;
